c09d4edfcf204d02f5fd34c79fe4743694894d46
anta <antandoye0@gmail.com>
2019-12-06 17:15:30 +0100

    modif

--- dateloc/localisation.py
+++ dateloc/localisation.py
@@ -7,14 +7,5 @@ def donnee_localisation():
     url = 'http://ipinfo.io/json'
     response = urlopen(url)
     data = json.load(response)
-
-    Adresse_IP=data['ip']
-    country=data['country']
-    latitude_longitude=data['loc']
-    Fuseaux_horraire = data['timezone']
-
-    message ='Détails des données de localisation\n\n'
-    data='Adresse_IP : {1} \nCountry : {0}  \nlatitude_longitude: {2} \nFuseaux_horraire: {3}'.format(country,Adresse_IP,latitude_longitude,Fuseaux_horraire)
-    infos= message + data
-    return infos
+    return data
 
